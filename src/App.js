0f595fee0fba03ed00cc1746beca1eb657f11301
Sushmitacode12 <sushmitatripathi786@gmail.com>
2023-04-27 19:23:03 +0530

    LoginPage Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import SignUP from './components/pages/signup';
- 
+
 function App() {
   return (
     <div>
       <Routes>
-      <Route path = '/' element= {<SignUP/>}/>
+      <Route path='/' element = {<Login/>}/>
+      <Route path = '/signup' element= {<SignUP/>}/>
+      <Route path = '/welcome' element = {<Welcome/>}/>
      </Routes>
     </div>
   );
