726c4ce4cbacf7ce63a05c64c5db646a848cc1a5
Sushmitacode12 <sushmitatripathi786@gmail.com>
2023-04-28 05:34:23 +0530

    Inbox page and logout butn added

--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -3,12 +3,22 @@ import EmailForm from './EmailForm'
 import Sentbox from './Sentbox' 
 
 function Welcome() {
+  const history = useNavigate()
+    const dispatch = useDispatch()
+    const logoutHandler = ()=>{
+        dispatch(authAction.Logout())
+        history('/')
+    }
   return (
     <div>
-    <div style={{textAlign:'center'}}>
-      <h2>welcome to mail box client</h2>
-    </div>
+    <div>
+      <h4>welcome to mail box client</h4>
     <hr/>
+    {<Button variant='outline-primary'><Link to='/sentbox'>sent box</Link></Button>}
+      {<Button variant='outline-primary' style={{marginLeft:'20px'}}><Link to='/inbox'>Inbox</Link></Button>}
+      <Button variant='danger' onClick={logoutHandler} style={{marginLeft:'20px'}}>Logout</Button>
+
+    </div>
     <EmailForm/>
    </div>
   )
